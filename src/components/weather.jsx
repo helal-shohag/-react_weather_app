c44b33e005eb1809c6f6821fe094e93b4ad7b63a
helal-shohag <shohag.2896@gmail.com>
2025-05-16 12:09:13 +0600

    css

---
+++ src/components/weather.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function weather() {
+  return (
+    <div className='weather'>
+        <h2 className='text'>Search Your City</h2>
+        <div>
+
+        </div>
+        <input type='text' placeholder='Search The City' />
+
+        <button>Search</button>
+    </div>
+  )
+}
+
+export default weather
